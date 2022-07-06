1963030e93bc683d9b24537ebddcbd28b5747b61
Szabolcs Gonczy <50486542+GSZB@users.noreply.github.com>
2022-07-06 16:29:05 +0200

    Update map.js
    updated the map

--- js/map.js
+++ js/map.js
@@ -11,7 +11,7 @@ const tokyoMap = L.map('map-canvas')
   })
   .setView(TOKYO_COORDINATES, 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox/streets-v11',
